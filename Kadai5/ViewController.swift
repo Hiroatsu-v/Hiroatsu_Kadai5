c50a36fc9440ce6d26cabd7f1f008de177854c89
Hiroatsu <hiroatsu.7190@gmail.com>
2021-09-30 10:05:13 +0900

    Initial Commit

---
+++ Kadai5/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Kadai5
+//
+//  Created by Hiroatsu on 2021/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var leftTextFiled: UITextField!
+    @IBOutlet private weak var rightTextFiled: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    @IBAction private func calculateButton(_ sender: UIButton) {
+        // nilチェックしながらアンラップ
+        guard let leftNumber = Double(leftTextFiled.text!) else {
+            addAlert(message: "割られる数を入力して下さい")
+            return
+        }
+        guard let rightNumber = Double(rightTextFiled.text!) else {
+            addAlert(message: "割る数を入力して下さい")
+            return
+        }
+        guard rightNumber != 0 else {
+            addAlert(message: "割る数に0を入力しないで下さい")
+            return
+        }
+        let resultNumber = String(leftNumber / rightNumber)
+        resultLabel.text = resultNumber
+    }
+    // AlertController
+    private func addAlert(message: String) {
+        let alertController = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
